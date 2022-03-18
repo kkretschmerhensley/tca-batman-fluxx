e6d1fb5b499c3bd02ba6c74c3dc4b6710a2364ed
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2022-03-18 13:27:54 -0500

    removed hard coded game data

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -17,27 +17,26 @@ interface CurrentGame {
   availablePlayers: Player[];
 }
 
-const game1: GameResult = {
-    start: "2022-02-14T15:14:30"
-    , end: "2022-02-14T15:20:00"
-    , winner: "Me"
-    , players: [{name: "Me", order: 1}, {name: "Marge", order: 2}, {name: "Bart", order: 3}, {name: "Lisa", order: 4}, {name: "Homer", order: 5}]
-};
-
-const game2: GameResult = {
-    start: "2022-02-14T21:00:30"
-    , end: "2022-02-14T21:30:30"
-    , winner: "Lisa"
-    , players: [{name: "Me", order: 1}, {name: "Marge", order: 2}, {name: "Bart", order: 3}, {name: "Lisa", order: 4}]
-};
+// const game1: GameResult = {
+//     start: "2022-02-14T15:14:30"
+//     , end: "2022-02-14T15:20:00"
+//     , winner: "Me"
+//     , players: [{name: "Me", order: 1}, {name: "Marge", order: 2}, {name: "Bart", order: 3}, {name: "Lisa", order: 4}, {name: "Homer", order: 5}]
+// };
+//
+// const game2: GameResult = {
+//     start: "2022-02-14T21:00:30"
+//     , end: "2022-02-14T21:30:30"
+//     , winner: "Lisa"
+//     , players: [{name: "Me", order: 1}, {name: "Marge", order: 2}, {name: "Bart", order: 3}, {name: "Lisa", order: 4}]
+// };
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  // calculate winning results using .filter
-  gameResults = [game1, game2];
+  gameResults = [];
 
   addGameResult = (r: GameResult) => {
     this.gameResults = [...this.gameResults, r]
