8d6eee00f506fc8bc5aef0e1c8ec38b639da201c
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2022-04-04 15:43:38 -0500

    Added Games Won and Games Lost component

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -14,20 +14,6 @@ interface CurrentGame {
   start: string;
 }
 
-// const game1: GameResult = {
-//     start: "2022-02-14T15:14:30"
-//     , end: "2022-02-14T15:20:00"
-//     , winner: "Me"
-//     , players: [{name: "Me", order: 1}, {name: "Marge", order: 2}, {name: "Bart", order: 3}, {name: "Lisa", order: 4}, {name: "Homer", order: 5}]
-// };
-//
-// const game2: GameResult = {
-//     start: "2022-02-14T21:00:30"
-//     , end: "2022-02-14T21:30:30"
-//     , winner: "Lisa"
-//     , players: [{name: "Me", order: 1}, {name: "Marge", order: 2}, {name: "Bart", order: 3}, {name: "Lisa", order: 4}]
-// };
-
 @Injectable({
   providedIn: 'root'
 })
@@ -38,10 +24,6 @@ export class GameService {
   addGameResult = (r: GameResult) => {
     this.gameResults = [...this.gameResults, r]
   };
-
-  // getUniquePlayers = () => (
-  //   [...new Set(this.gameResults.flatMap(x => x.players.map(y => y.name)))]
-  // );
 
   currentGame: CurrentGame = {
     start: ""
